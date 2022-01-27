41b49f5c6c67890af4031df4f773aa5842a9ed95
salarshirkhani <salarshirkhany16@gmail.com>
2022-01-27 22:56:01 +0330

    add api

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/licenses', function(){
+    return ['message' => 'hello'];
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
